302a3f4f64c0cd2eda15d908b8d55834780eb785
erickb88 <kelerman.eric@gmail.com>
2022-12-01 10:48:54 +0200

    Second commit

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,2 +1,29 @@
-package base;public class BaseTests {
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+    public class BaseTests {
+    public WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\keler\\Desktop\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+    }
+
+    public static void main(String[] args) {
+        BaseTests tests = new BaseTests();
+        tests.setUp();
+
+    }
 }
